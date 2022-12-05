1d562bcbf570c91c2763c67c5ddd7184342b7556
refszin <refwire@gmail.com>
2022-12-05 13:15:44 -0300

    Feat: Booking Services

--- src/services/booking-service/index.ts
+++ src/services/booking-service/index.ts
@@ -1,45 +1,85 @@
-import hotelRepository from "@/repositories/hotel-repository";
-import enrollmentRepository from "@/repositories/enrollment-repository";
 import bookingRepository from "@/repositories/booking-repository";
+import enrollmentRepository from "@/repositories/enrollment-repository";
+import hotelRepository from "@/repositories/hotel-repository";
+import { notFoundError, forbiddenError } from "@/errors";
 import ticketRepository from "@/repositories/ticket-repository";
-import { notFoundError } from "@/errors";
-import { cannotListHotelsError } from "@/errors/cannot-list-hotels-error";
 
 async function getBooking(userId: number) {
-  //Tem enrollment?
-  const enrollment = await bookingRepository.findWithAddressByUserId(userId);
-  if (!enrollment) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  const booking = await bookingRepository.findBookingByUserId(userId);
+
+  if(!booking || !enrollment) {
     throw notFoundError();
   }
-  //Tem ticket pago isOnline false e includesHotel true
+
+  return booking;
+}
+
+async function createBooking(roomId: number, userId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  const room = await hotelRepository.findRoomById(roomId);
+
+  if(!room || !enrollment) {
+    throw notFoundError();
+  }
+  
   const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
 
-  if (!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
-    throw cannotListHotelsError();
+  if(!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
+    throw forbiddenError();
   }
+
+  const booking = await bookingRepository.findBookingByUserId(userId);
+  const capacity = await bookingRepository.findBookingCountByRoomId(roomId);
+
+  if(capacity[0]?._count.roomId === room.capacity || booking) {
+    throw forbiddenError();
+  }
+
+  await bookingRepository.create(roomId, userId);
+  
+  const result = await bookingRepository.findBookingByUserId(userId);
+
+  return result; 
 }
 
-async function postBooking(userId: number, newBooking: object) {
-  await listHotels(userId);
+async function updateBooking(roomId: number, userId: number, bookingId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  const room = await hotelRepository.findRoomById(roomId);
 
-  const hotels = await bookingRepository.findHotels();
-  return hotels;
-}
-
-async function putBooking(userId: number, hotelId: number) {
-  await listHotels(userId);
-  const hotel = await bookingRepository.findRoomsByHotelId(hotelId);
-
-  if (!hotel) {
+  if(!room || !enrollment) {
     throw notFoundError();
   }
-  return hotel;
+  
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+
+  if(!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
+    throw forbiddenError();
+  }
+
+  const bookingByUserId = await bookingRepository.findBookingByUserId(userId);
+  const capacity = await bookingRepository.findBookingCountByRoomId(roomId);
+  const bookingById = await bookingRepository.findBookingById(bookingId);
+
+  if(capacity[0]?._count.roomId === room.capacity || !bookingByUserId || !bookingById) {
+    throw forbiddenError();
+  }
+
+  if(roomId === bookingByUserId.Room.id) {
+    throw forbiddenError();
+  }
+
+  await bookingRepository.update(roomId, bookingId);
+  
+  const result = await bookingRepository.findBookingByUserId(userId);
+
+  return result; 
 }
 
 const bookingService = {
   getBooking,
-  postBooking,
-  putBooking,
+  createBooking,
+  updateBooking,
 };
 
 export default bookingService;
